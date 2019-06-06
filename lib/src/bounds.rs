8e034d679f0668889b3af37f80f87ff8257abbde
MaxenceHanin <hanin@etud.insa-toulouse.fr>
2019-06-06 18:32:59 +0200

    implementing func to return evacsolution instead of u32 for bound min

--- lib/src/bounds.rs
+++ lib/src/bounds.rs
@@ -1,5 +1,8 @@
 use crate::roads::RoadEdge;
 use crate::roads::RoadNetwork;
+use crate::checker::EvacuationSolution;
+use crate::evac::EvacuationNode;
+use std::time::Instant;
 
 //recursive func to find all the child, add their length and return the total
 pub fn next_node(tree: RoadNetwork, next: Option<RoadEdge>, time: u32, _length: u32) -> u32 {
@@ -33,6 +36,65 @@ pub fn bound_inf(tree: RoadNetwork) -> u32 {
         }
     }
     return result_final;
+}
+
+//find max evac_rate for each evac node
+pub fn vec_node(tree: RoadNetwork, mut vec:Vec<u32>, node: EvacuationNode ) -> Vec<u32> {
+    //println!("time: {}", time);
+    let mut next_node = node.clone();
+    match tree.clone().get_child_edge(node.id) {
+        // There is a child
+        Some(x) => {
+            vec.push(x.length);
+            for node in tree.clone().evac_info.nodes {
+                if x.child == node.id{
+                    next_node = node;
+                }
+            }
+
+            vec_node(
+                tree.clone(),
+                vec,
+                next_node,
+            )
+        },
+        // safe node reached
+        None => vec,
+    }
+}
+pub fn find_max_evac_rate(tree: RoadNetwork, node: EvacuationNode) -> (u32,u32) {
+    let mut tot = 0;
+    let mut res = Vec::new();
+    let mut vec =vec_node(tree.clone(), res, node);
+    vec.sort();
+    let mut vec_len =vec.len()-1;
+    while vec_len >=0 {
+        tot = tot+vec[vec_len];
+        vec_len = vec_len -1;
+    }
+    return (vec[0],tot);
+}
+
+//bound inf but returning EvacuationSolution
+pub fn bound_inf_evac_sol(tree: RoadNetwork) -> EvacuationSolution {
+
+    let earlier = Instant::now();
+    let mut nb =0;
+    let mut start_evac =0;
+    let mut max_ev_rate =(0,0);
+    let mut evac_solution = EvacuationSolution::new("solution");
+    evac_solution.valid = true;
+    evac_solution.goal_value = bound_inf(tree.clone()) as f32;
+
+    for node in tree.clone().evac_info.nodes {
+        max_ev_rate =find_max_evac_rate(tree.clone(), node.clone());
+        nb = (node.population / max_ev_rate.1)+1;
+        evac_solution.add_node(node.id, max_ev_rate.0+nb, start_evac);
+        start_evac = start_evac + max_ev_rate.0+nb;
+    }
+    let now = Instant::now();
+    evac_solution.compute_time = now.duration_since(earlier).as_secs() as f32;
+    return evac_solution;
 }
 
 //bound sup is the total of node's evacuation time, if they are conidered as alone
